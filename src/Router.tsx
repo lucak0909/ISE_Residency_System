dc4a36ddfa00f768c08b1da4db01118b747cce29
lucak0909 <lucak0909@gmail.com>
2025-06-04 13:23:36 +0100

    General Cleaning

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,24 +1,18 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
-import Login             from './pages/App';
+import Login             from './pages/AuthPage';
 import JobsBoard         from './pages/JobsBoard';
-
 import StudentDashboard  from './pages/S_Dashboard';
 import StudentRanking1   from './pages/S_Ranking1';
 import StudentRanking2   from './pages/S_Ranking2';
-
 import PartnerDashboard  from './pages/P_Dashboard';
 import PartnerRanking    from './pages/P_Ranking';
-
-//import AdminDashboard  from './pages/A_Dashboard.tsx';
-//import AdminDatabase    from './pages/A_Database.tsx';
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 {/* public */}
-                <Route path="/login" element={<Login />} />
+                <Route path="/Login" element={<Login />} />
                 <Route path="/JobsBoard" element={<JobsBoard/>}/>
 
                 {/* student area */}
@@ -29,12 +23,6 @@ export default function Router() {
                 {/* partner area */}
                 <Route path="/PartnerDashboard" element={<PartnerDashboard />} />
                 <Route path="/PartnerRanking"  element={<PartnerRanking  />} />
-
-                {/* admin area */}
-                {/*
-                <Route path="/AdminDashboard" element={<AdminDashboard />} />
-                <Route path="/AdminDatabase"  element={<AdminDatabase  />} />
-                */}
 
                 {/* shortcuts / fall-backs */}
                 <Route path="/"         element={<Navigate to="/Login" />} />
